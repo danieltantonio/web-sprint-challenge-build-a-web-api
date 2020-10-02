d1d46f1f131b54afdf3b3ee1a58f06429063f0b3
Daniel Antonio <dernlsenpai@gmail.com>
2020-10-02 10:29:35 -0700

    typo on process.env for PORT

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const server = require('./server');
 
 server.listen(port, () => console.log(`Server started on port: ${port}...`));
